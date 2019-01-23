6a6944c73538d3f921a2e2069f4596eeb30eca13
Zeyad-37 <zeyad.gasser@gmail.com>
2019-01-23 18:00:22 +0100

    testing

--- auto_deployment/release.sh
+++ auto_deployment/release.sh
@@ -3,8 +3,8 @@
 function gh_create_release {
     tag_name=$1
     body=$2
-    jbody= "${body//$'\n'/\n}"
-#    jbody= "'"${body//$'\n'/'\n'}"'"
+    jbody="${body//$'\n'/\n}"
+#    jbody="'"${body//$'\n'/'\n'}"'"
     githubAPIToken=$3
     echo ${jbody}
     API_JSON="{\"tag_name\": \"$tag_name\",
